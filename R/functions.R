6a871a1ca9b8ccc550545db239b29203eaec1c76
Pierrick Bruneau <pbruneau@gmail.com>
2013-09-16 00:00:00 +0000

    version 0.1.1

--- R/functions.R
+++ R/functions.R
@@ -50,34 +50,38 @@ speccalt <- function(kern, k=NA, maxk=20) {
 	
 }
 
+
 bartlett <- function(eigvals, thres=0.95, maxk=20) {	 # adaptation of Bartlett statistical test for equal variances
-	found <- FALSE
-	n <- length(eigvals)
+	n <- length(eigvals) - 1
+	m <- length(eigvals)
 	# thres eigenvals to 10^{-12}
 	eigvals[eigvals<10^{-12}] <- 10^{-12}
 	stats <- numeric()
 	tests <- numeric()
 	# as we exclude the last eigenval, we start the tests at m=2 - which naively always has 0 tstat.
-	for(m in 2:maxk) {
-		k <- n-m+1
-		nu <- (m-1) * (m+2) / 2		
-		Vk <- (n-1)-(k-1) - (2 * m^2 + 2) / (6 * m) + sum(mean(eigvals[k:(n-1)])^2 / (eigvals[k:(n-1)] - mean(eigvals[1:(k-1)]))^2)
-		tstat <- prod((m-1) * eigvals[k:(n-1)] / sum(eigvals[k:(n-1)]))
-		tstat <- -Vk * log(tstat)
+	for(p in 2:maxk) {
+		q <- m-p
+		nu <- (p-1) * (p+2) / 2
+		# approx : with such laplacians, eigenvals up to rank k are close to 0,
+		# so we approximate these by 1.
+		fact <- n-q - (2 * p^2 + 2) / (6 * p) + q * (mean(eigvals[(q+1):n])^2) / ((1 - mean(eigvals[(q+1):n]))^2)
+		Vk <- prod((p-1) * eigvals[(q+1):n] / sum(eigvals[(q+1):n]))
+		tstat <- -fact * log(Vk)
 		stats <- c(stats, tstat)
 		tests <- c(tests, pchisq(tstat, nu))
 	}
 	
 	# if thres is exceeded -> return the preceding index
-	# else : return the index of the max,
+	# if not exceeded, return the index preceding the max of the stat.
 	if(any(tests > thres)) {
 		thresind <- which(tests>thres)[1]
 	} else {
-		thresind <- which.max(tests)+1
+		thresind <- which.max(tests)
 	}
 	
 	return(list(k=thresind, stats=stats, tests=tests))
 }
+
 
 
 
